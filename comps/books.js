5800fb0770586f55feb783b9bf385b14250264b9
sergeyradkov <radkov@gmail.com>
2016-05-10 12:02:32 -0600

    final

--- comps/books.js
+++ comps/books.js
@@ -1,6 +1,5 @@
 angular.module('books').component('bookStore', {
     templateUrl: 'comps/books.html',
-    controllerAs: 'bs',
     controller: function($scope, $firebaseArray){
         var bs = this;
         bs.books = [];
@@ -9,14 +8,19 @@ angular.module('books').component('bookStore', {
         bs.books = $firebaseArray(library);
         
         bs.addBook = function (book){
-            bs.books.$add(book);
-            $scope.book = {};
+          if(book.$id){ bs.books.$save(book);
+            }else
+                {bs.books.$add(book)
+                }
+            bs.newBook = null;
         }
+        
         bs.removeBook = function(book){
             bs.books.$remove(book);
         }
+        
         bs.editBook = function (book){
-            
+            bs.newBook = book;
         }
     }
 })
